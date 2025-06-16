4f9bc26eec1e7e75bf5c5225a74e53508a5af2d5
Lucas Rodrigues <lucasrodrigues.eng@outlook.com>
2025-06-15 21:57:50 -0300

    Desafio concluido!

--- Program.cs
+++ Program.cs
@@ -6,20 +6,34 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+int numeroHospedes = 4;
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+for (int i = 0; i < numeroHospedes; i++)
+{
+    Pessoa hospede = new Pessoa(nome: $"Hóspede {i + 1}");
+    hospedes.Add(hospede);
+}
+
+//Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+//Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+//Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+//Pessoa p4 = new Pessoa(nome: "Hóspede 4");
+
+//hospedes.Add(p1);
+//hospedes.Add(p2);
+//hospedes.Add(p3);
+//hospedes.Add(p4);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+
+Console.ReadKey();
